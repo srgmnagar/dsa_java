f027744e662f602562861e4b80e258d41279876d
srgmnagar <nagarsrgm@gmail.com>
2025-08-19 21:01:52 +0530

    Refactor Fibonacci implementation to use formula for calculation

--- recurrsion/fib.java
+++ recurrsion/fib.java
@@ -1,9 +1,13 @@
 class fib {
     public static void main(String[] args) {
         // Your code
-        int ans=fib(2);
+        int ans=fibformula(50);
         System.out.println(ans);
     }
+    static int fibformula(int n){
+        return (int)((Math.pow(((1+Math.sqrt(5))/2),n)-Math.pow(((1-Math.sqrt(5))/2),n))/Math.sqrt(5));
+    }
+
     static int fib(int n){
         if(n==0 || n==1){
             return n;
