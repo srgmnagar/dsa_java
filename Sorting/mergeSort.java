0c4ac98ac42ed99024fba1b4143d223b78dd5991
srgmnagar <nagarsrgm@gmail.com>
2026-01-20 00:38:24 +0530

    Add implementation of merge sort algorithm

---
+++ Sorting/mergeSort.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class mergeSort {
+    public static void main(String[] args) {
+        int arr[]={5,2,1,6,3,4};
+        int[] sorted=mergeSort(arr);
+        System.out.println(Arrays.toString(sorted));
+
+    }
+    static int[] mergeSort(int[] arr){
+        if(arr.length <= 1) return arr;
+        int mid=arr.length/2;
+        int[] left=mergeSort(Arrays.copyOfRange(arr,0,mid));
+        int[] right=mergeSort(Arrays.copyOfRange(arr,mid,arr.length));
+        return merge(left,right);
+    }
+    static int[] merge(int[] left,int[] right){
+        int[] result=new int[left.length+right.length];
+        int i=0,j=0,k=0;
+        while(i<left.length && j<right.length){
+            if(left[i]<right[j]){
+                result[k++]=left[i++];
+            }else{
+                result[k++]=right[j++];
+            }
+        }
+        while(i<left.length){
+            result[k++]=left[i++];
+        }
+        while(j<right.length){
+            result[k++]=right[j++];
+        }
+        return result;
+    }
+    }
+
