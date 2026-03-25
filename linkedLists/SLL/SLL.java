9da11baebccd91d51b8eb686961052677db03ce8
Sargam <nagarsrgm@gmail.com>
2026-03-25 23:43:59 +0530

    Add reverse method to SLL for reversing the linked list

--- linkedLists/SLL/SLL.java
+++ linkedLists/SLL/SLL.java
@@ -110,6 +110,16 @@ public class SLL{
         return node;
     }
 
+    public void reverse(Node node){
+        if(node==tail){
+            head=tail;
+            return;
+        }
+        reverse(node.next);
+        tail.next=node;
+        tail=node;
+        tail.next=null;
+    }
     public void bubble(){
         bubbleSort(size-1, 0);
     }
